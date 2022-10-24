5868ba851117881629ad6d3612525342933f214b
G.Chandravadan Reddy <84029812+vadanreddy88@users.noreply.github.com>
2022-10-24 11:09:22 +0530

    n=int(input()) sum=0 for i in range(1,n):     if(n%i==0):         sum=sum+i if(sum==n):     print("True") else:     print("False")

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,12 +1,9 @@
-def ispositive(n):
-    s=0
-    for i in range(1,n):
-        if n%i==0:
-            s+=i
-    if s==n:
-        return True
-    else:
-        return False
 n=int(input())
-res=ispositive(n)
-print(res)
+sum=0
+for i in range(1,n):
+    if(n%i==0):
+        sum=sum+i
+if(sum==n):
+    print("True")
+else:
+    print("False")
